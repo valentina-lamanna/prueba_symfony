1f36e3c593abfb973b9269e553049dbaff4954e5
vlamannaantico <vlamannaantico@frba.utn.edu.ar>
2022-08-31 15:35:34 -0300

    /books

--- symfony/src/Controller/LibraryController.php
+++ symfony/src/Controller/LibraryController.php
@@ -2,43 +2,72 @@
 
 namespace App\Controller;
 
+use App\Entity\Book;
+use App\Repository\BookRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 use Psr\Log\LoggerInterface;
+use Doctrine\ORM\EntityManagerInterface;
 
 class LibraryController extends AbstractController
 {
 
     /**
-    *@Route("/library/list", name= "library_list");
-    */
+     *@Route("/books", name= "book_get");
+     */
 
-    public function library_list(Request $request){
+    public function library_list(Request $request, BookRepository  $bookRepository){
 
-        $title = $request->get('title', 'Alegria');
 //      this->logger->info('List action called');
+        $books = $bookRepository->findAll();
+        $booksAsArray=[];
+        foreach($books as $book){
+            $booksAsArray[]= [
+                'id' => $book->getId(),
+                'title' => $book->getTitle(),
+                'image' => $book->getImage()
+            ];
+        };
         $response = new JsonResponse();
         $response->setData([
             'succes' => true,
-            'data' =>[
-                [
-                    'id'=> 1,
-                    'title' => 'Hola mundo'
-                ],
-                [
-                    'id' => 2,
-                    'title' => 'Libro 2'
-                ],
-                [
-                    'id' => 3,
-                    'title' => $title
-                ]
-            ]
+            'data' => $booksAsArray
         ]);
 
         return $response;
     }
 
+    /**
+     * @Route("/book/create", name="create_book");
+     */
+
+    public function createBook(Request $request, EntityManagerInterface $em){
+
+        $book = new Book();
+        $title= $request->get('title',null);
+        $response = new JsonResponse();
+        if(empty($title)){
+            $response->setData([
+                'succes' => false,
+                'error' => 'El titulo no puede ir vacio',
+                'data' => null
+            ]);
+            return $response;
+        }
+        $book->setTitle($title);
+        $em->persist($book);
+        $em->flush();
+        $response->setData([
+            'succes' => true,
+            'data' =>[
+                [
+                    'id'=> $book->getId(),
+                    'title' => $book->getTitle()
+                ]
+            ]
+        ]);
+        return $response;
+    }
 }
