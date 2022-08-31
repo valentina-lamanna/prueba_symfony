7a2a5109d18681134249be57d51f195b73e88764
vlamannaantico <vlamannaantico@frba.utn.edu.ar>
2022-08-31 17:28:02 -0300

    update- get_byID- delete

--- symfony/src/Controller/LibraryController.php
+++ symfony/src/Controller/LibraryController.php
@@ -4,12 +4,15 @@ namespace App\Controller;
 
 use App\Entity\Book;
 use App\Repository\BookRepository;
+use http\Env\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 use Psr\Log\LoggerInterface;
 use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\Persistence\ManagerRegistry;
+
 
 class LibraryController extends AbstractController
 {
@@ -18,7 +21,7 @@ class LibraryController extends AbstractController
      *@Route("/books", name= "book_get");
      */
 
-    public function library_list(Request $request, BookRepository  $bookRepository){
+    public function book_list(Request $request, BookRepository  $bookRepository){
 
 //      this->logger->info('List action called');
         $books = $bookRepository->findAll();
@@ -69,5 +72,95 @@ class LibraryController extends AbstractController
             ]
         ]);
         return $response;
+    }
+
+    /**
+     * @Route("/book/{id}", name="book_get_id");
+     */
+
+    public function  get_book_by_id(BookRepository  $bookRepository, int $id){
+        $book= $bookRepository->find($id);
+
+        if (!$book) {
+            throw $this->createNotFoundException(
+                'No book found for id '.$id
+            );
+        }
+        $response= new JsonResponse();
+        $response->setData([
+            'succes' => true,
+            'data' =>[
+                [
+                    'id'=> $book->getId(),
+                    'title' => $book->getTitle()
+                ]
+            ]
+        ]);
+        return $response;
+
+    }
+
+    /**
+     * @Route("/book/edit/{id}", name="book_update");
+     */
+    public function update(EntityManagerInterface $em, int $id, BookRepository  $bookRepository , Request $request){
+
+        $book= $bookRepository->find($id);
+        if(!$book){
+            throw $this->createNotFoundException(
+                'Not book was found for id '. $id
+            );
+        }
+        $response = new JsonResponse();
+        $title =$request->get('title');
+        if(empty($title)){
+            $response->setData([
+                'succes' => false,
+                'error' => 'El titulo no puede ir vacio',
+                'data' => null
+            ]);
+            return $response;
+        }
+
+        $book->setTitle($title);
+        $em->persist($book);
+        $em->flush();
+
+        $response->setData([
+            'succes' => true,
+            'data' =>[
+                [
+                    'id'=> $book->getId(),// $id
+                    'title' => $book->getTitle()
+                ]
+            ]
+        ]);
+        return $response;
+
+    }
+    /**
+     * @Route("/book/delete/{id}", name="book_update");
+     */
+    public function delete(EntityManagerInterface $em, int $id, BookRepository  $bookRepository){
+
+        $book= $bookRepository->find($id);
+        if(!$book){
+            throw $this->createNotFoundException(
+                'Not book was found for id '. $id
+            );
+        }
+        $response = new JsonResponse();
+
+
+
+        $em->remuve($book);
+        $em->flush();
+
+        $response->setData([
+            'succes' => true,
+            'data' =>['se elimino correctamnete el libro con id '. $id]
+        ]);
+        return $response;
+
     }
 }
